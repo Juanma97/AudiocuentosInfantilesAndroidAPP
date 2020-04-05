2556e6cff9843a5a4bf3a18859256e5e5f64ff85
Juan Manuel Perez Toro <juanma97@outlook.com>
2020-04-05 13:02:59 +0100

    Add mediaplayer

--- app/src/main/java/com/alpha/audiocuentosinfantiles/MainActivity.kt
+++ app/src/main/java/com/alpha/audiocuentosinfantiles/MainActivity.kt
@@ -15,6 +15,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import android.media.MediaPlayer
 
 
 class MainActivity : AppCompatActivity() {
